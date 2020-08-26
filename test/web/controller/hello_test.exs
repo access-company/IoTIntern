8d3d8b9bf17797eb9ad8956d7fdb7937cfafd853
Atsushi Narusawa <atsushi.narusawa@access-company.com>
2020-08-26 19:46:34 +0900

    Add meck example

--- test/web/controller/hello_test.exs
+++ test/web/controller/hello_test.exs
@@ -1,25 +1,32 @@
 defmodule IotIntern.Controller.HelloTest do
   use ExUnit.Case
 
+  alias Antikythera.Httpc
+
   @api_path "/api/v1/hello"
 
   test "hello api succeeds with 200" do
-    headers = %{"auth" => "xxxx"}
+    # NOTE: モジュール名、関数名、引数の数, 任意の返り値
+    :meck.expect(Httpc, :get, 1, :ok)
 
+    headers = %{"auth" => "xxxx"}
     Enum.each(["hello", "world"], fn message ->
       req_body = %{"message" => message}
       res = Req.post_json(@api_path, req_body, headers)
       assert res.status == 200
       assert Jason.decode!(res.body) == %{"message" => message}
     end)
+    :meck.unload()
   end
 
   test "hello api fails with 400" do
+    :meck.expect(Httpc, :get, 1, :ok)
     headers = %{"auth" => "xxxx"}
     req_body = %{"message" => "Invalid Message"}
     res = Req.post_json(@api_path, req_body, headers)
     assert res.status == 400
     assert Jason.decode!(res.body) == %{"message" => "ng"}
+    :meck.unload()
   end
 
   test "hello-croma api succeeds with 200" do
--- web/controller/hello.ex
+++ web/controller/hello.ex
@@ -1,7 +1,7 @@
 defmodule IotIntern.Controller.Hello do
   use Antikythera.Controller
 
-  alias Antikythera.Conn
+  alias Antikythera.{Conn, Httpc}
 
   def hello(%{request: req} = conn) do
     %{
@@ -12,6 +12,9 @@ defmodule IotIntern.Controller.Hello do
         "message" => msg,
       } = req_body,
     } = req
+
+    # NOTE: モックして ok を返すようにする
+    :ok = Httpc.get("https://example.com")
 
     if auth == "xxxx" and (msg == "hello" or msg == "world") do
       Conn.json(conn, 200, req_body)
