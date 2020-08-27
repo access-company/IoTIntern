d3d1f4155183f0fdbd7fcf5fe321e1febd2c5303
Atsushi Narusawa <atsushi.narusawa@access-company.com>
2020-08-27 13:29:27 +0900

    Use seup block

--- test/web/controller/hello_test.exs
+++ test/web/controller/hello_test.exs
@@ -5,6 +5,10 @@ defmodule IotIntern.Controller.HelloTest do
 
   @api_path "/api/v1/hello"
 
+  setup do
+    on_exit(&:meck.unload/0)
+  end
+
   test "hello api succeeds with 200" do
     # NOTE: モジュール名、関数名、引数の数, 任意の返り値
     :meck.expect(Httpc, :get, 1, :ok)
@@ -16,7 +20,6 @@ defmodule IotIntern.Controller.HelloTest do
       assert res.status == 200
       assert Jason.decode!(res.body) == %{"message" => message}
     end)
-    :meck.unload()
   end
 
   test "hello api fails with 400" do
@@ -26,6 +29,5 @@ defmodule IotIntern.Controller.HelloTest do
     res = Req.post_json(@api_path, req_body, headers)
     assert res.status == 400
     assert Jason.decode!(res.body) == %{"message" => "ng"}
-    :meck.unload()
   end
 end
