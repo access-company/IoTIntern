f6ea8561446b60a07dcda70a8027d32ec0046968
Atsushi Narusawa <atsushi.narusawa@access-company.com>
2020-09-02 15:04:10 +0900

    [#213167] API 追加のやり方の例を追加 (#4)

--- test/web/controller/hello_test.exs
+++ test/web/controller/hello_test.exs
@@ -1,12 +1,33 @@
 defmodule IotIntern.Controller.HelloTest do
   use ExUnit.Case
 
-  test "hello should render HAML template as HTML" do
-    response = Req.get("/hello", %{}, [params: %{"locale" => "ja"}])
-    assert response.status == 200
-    assert response.headers["content-type"] == "text/html; charset=utf-8"
-    body = response.body
-    assert String.starts_with?(body, "<!DOCTYPE html>")
-    assert String.contains?(body, "Message from iot_intern: こんにちは")
+  alias Antikythera.Httpc
+
+  @api_path "/api/v1/hello"
+
+  setup do
+    on_exit(&:meck.unload/0)
+  end
+
+  test "hello api succeeds with 200" do
+    # NOTE: モジュール名、関数名、引数の数, 任意の返り値
+    :meck.expect(Httpc, :get, fn _url -> :ok end)
+
+    headers = %{"auth" => "xxxx"}
+    Enum.each(["hello", "world"], fn message ->
+      req_body = %{"message" => message}
+      res = Req.post_json(@api_path, req_body, headers)
+      assert res.status == 200
+      assert Jason.decode!(res.body) == %{"message" => message}
+    end)
+  end
+
+  test "hello api fails with 400" do
+    :meck.expect(Httpc, :get, 1, :ok)
+    headers = %{"auth" => "xxxx"}
+    req_body = %{"message" => "Invalid Message"}
+    res = Req.post_json(@api_path, req_body, headers)
+    assert res.status == 400
+    assert Jason.decode!(res.body) == %{"message" => "ng"}
   end
 end
