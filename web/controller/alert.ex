9b7907c258253ce9c3b378240a2ecccbaa3f1d81
Atsushi Narusawa <atsushi.narusawa@access-company.com>
2020-10-27 11:33:50 +0900

    Change api impl

--- web/controller/alert.ex
+++ web/controller/alert.ex
@@ -1,11 +1,9 @@
 # 課題1
 #   以下のコードは空のレスポンスをステータス200で返すようになっています．
-#   仕様にそったレスポンスの一例を返すようにAPIを実装してください．
-#   （この課題ではリクエストに応じてレスポンスを変える必要はありません）
+#   仕様に沿って時刻を返すようにAPIを実装してください．
 #
 # 課題2
-#   課題1を元に，リクエストボディに応じてレスポンスが変わるように実装を変更してください．
-#   ただし，33行目から37行目のコメントアウトを解除して用いること．
+#   無効なリクエストボディの場合にレスポンス400が返るようにしてください．
 #   ヒント：リクエストボディは%{body: _body}を%{body: body}とするとbodyをコードで利用することが出来ます
 #
 # 課題3
@@ -15,7 +13,7 @@
 # 課題4
 #   テストコードを書いてみてください.
 #
-# 課題6
+# 課題6 (Optional)
 #   Cromaによるバリデーション機能を試してみてください.
 
 defmodule IotIntern.Controller.Alert do
@@ -25,13 +23,6 @@ defmodule IotIntern.Controller.Alert do
   # 必要に応じて適宜エイリアスのコメントアウトを解除してください
   # alias IotIntern.Error
   # alias IotIntern.Linkit
-
-  # アトリビュートのMapの中は好きに変更して構いません．
-  # @alert_messages %{
-  #   "dead_battery" => "バッテリー不足",
-  #   "derailment"   => "脱輪",
-  #   "jamming"      => "異物混入"
-  # }
 
   def post_alert(%{request: %{body: _body}} = conn) do
     Conn.json(conn, 200, %{})
