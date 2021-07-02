7e17d30c699ae9a54aa3db7278565fbb9efc9f11
hnishi <nishigami2509@gmail.com>
2021-07-02 16:52:39 +0900

    [229314] refactor test

--- test/web/controller/alert_test.exs
+++ test/web/controller/alert_test.exs
@@ -2,6 +2,7 @@ defmodule IotIntern.Controller.AlertTest do
   use ExUnit.Case
 
   alias Antikythera.Httpc
+  alias IotIntern.Linkit
 
   @api_path "/api/v1/alert"
 
@@ -10,33 +11,39 @@ defmodule IotIntern.Controller.AlertTest do
   end
 
   test "alert api succeeds with 200" do
-    res_body = """
-    {
-      "chat_message": {
-        "_id": "2456598c88d3fdd8aa000017",
-        "chat_room_id": "33398950c711636832000059",
-        "data": {},
-        "importance": "normal",
-        "is_deleted": false,
-        "message": "脱輪",
-        "msg_seq": 10230,
-        "post_date": "2015-07-22T00:57:45Z",
-        "request_id": "d80979c1-624c-4b9e-91e3-d1a0a713c7d01437526664419",
-        "type": "string",
-        "unread_count": 1,
-        "user_id": "1267712707"
+    res_body = %{
+      "chat_message" => %{
+        "_id" => "2456598c88d3fdd8aa000017",
+        "chat_room_id" => "33398950c711636832000059",
+        "data" => %{},
+        "importance" => "normal",
+        "is_deleted" => false,
+        "message" => "脱輪",
+        "msg_seq" => 10230,
+        "post_date" => "2015-07-22T00:57:45Z",
+        "request_id" => "d80979c1-624c-4b9e-91e3-d1a0a713c7d01437526664419",
+        "type" => "string",
+        "unread_count" => 1,
+        "user_id" => "1267712707"
       },
-      "server_time": "2015-07-22T00:57:45Z"
+      "server_time" => "2015-07-22T00:57:45Z"
     }
-    """
-
-    :meck.expect(Httpc, :post, 3, {:ok, %{status: 201, body: res_body}})
 
     now = ~U[2021-06-02T03:53:30Z]
     :meck.expect(DateTime, :utc_now, fn -> now end)
 
-    Enum.each(["jamming", "derailment", "dead_battery"], fn message ->
-      req_body = %{"type" => message}
+    [
+      {"jamming", "異物混入"},
+      {"derailment", "脱輪"},
+      {"dead_battery", "バッテリー不足"},
+    ]
+    |> Enum.each(fn {alert_message, linkit_message} ->
+      :meck.expect(Linkit, :post_message, fn message ->
+        assert message == linkit_message
+        {201, put_in(res_body, ["chat_message", "message"], linkit_message)}
+      end)
+
+      req_body = %{"type" => alert_message}
       res = Req.post_json(@api_path, req_body, %{})
       assert res.status == 200
       assert Jason.decode!(res.body) == %{"sent_at" => "2021-06-02T03:53:30Z"}
