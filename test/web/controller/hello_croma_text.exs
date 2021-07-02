b50324a54468d021bf41fef7f2d0872aef46d0e4
hnishi <nishigami2509@gmail.com>
2021-07-02 17:39:42 +0900

    merge updates of exmaple branch into master

--- test/web/controller/hello_croma_text.exs
+++ test/web/controller/hello_croma_text.exs
@@ -8,7 +8,7 @@ defmodule IotIntern.Controller.HelloTest do
   test "hello-croma api succeeds with 200" do
     headers = %{"auth" => "xxxx"}
     req_body = %{"message" => "hello"}
-    res = Req.post_json("/api/v1/hello-croma", req_body, headers)
+    res = Req.post_json(@api_path, req_body, headers)
     assert res.status == 200
     assert Jason.decode!(res.body) == %{"message" => "hello"}
   end
@@ -16,7 +16,7 @@ defmodule IotIntern.Controller.HelloTest do
   test "hello-croma api fails with 400" do
     headers = %{"auth" => "xxxx"}
     req_body = %{"message" => "Invalid Message"}
-    res = Req.post_json("/api/v1/hello-croma", req_body, headers)
+    res = Req.post_json(@api_path, req_body, headers)
     assert res.status == 400
     assert Jason.decode!(res.body) == %{"message" => "ng"}
   end
