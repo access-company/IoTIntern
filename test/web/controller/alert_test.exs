bcbf593195e442d684bccfc1fb628ce51e65efb4
hnishi <nishigami2509@gmail.com>
2021-09-15 16:51:55 +0900

    [234823] add a template file of task4

---
+++ test/web/controller/alert_test.exs
@@ -0,0 +1,16 @@
+# test/web/controller/hello_test.exs を参考に実装してください。
+
+# defmodule IotIntern.Controller.AlertTest do
+#   use ExUnit.Case
+
+#   setup do
+#     on_exit(&:meck.unload/0)
+#   end
+
+#   test "alert api succeeds with 200" do
+#     now = ~U[2021-06-02T03:53:30Z]
+#     :meck.expect(DateTime, :utc_now, fn -> now end)
+
+#     # Linkit API を meck してください。
+#   end
+# end
