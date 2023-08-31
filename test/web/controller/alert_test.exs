094d3849f58736877afd06d3216619db2d54f09e
Airi Kaneko <airi.kaneko@access-company.com>
2023-08-31 15:31:56 +0900

    task4
    (cherry picked from commit 9f5f4f83d7a293adbe330fbbe45663b69173f1c1)

--- test/web/controller/alert_test.exs
+++ test/web/controller/alert_test.exs
@@ -1,18 +1,85 @@
-# test/web/controller/hello_test.exs を参考に実装してください。
+defmodule IotIntern.Controller.AlertTest do
+  use ExUnit.Case
 
-# defmodule IotIntern.Controller.AlertTest do
-#   use ExUnit.Case
+  alias Antikythera.Httpc
+  alias IotIntern.Linkit
 
-#   setup do
-#     on_exit(&:meck.unload/0)
-#   end
+  @api_path "/api/v1/alert"
 
-#   test "alert api succeeds with 200" do
-#     now = ~U[2021-06-02T03:53:30Z]
-#     :meck.expect(DateTime, :utc_now, fn -> now end)
+  setup do
+    on_exit(&:meck.unload/0)
+  end
 
-#     # Linkit API を meck してください。
+  test "alert api succeeds with 200" do
+    res_body = %{
+      "chat_message" => %{
+        "_id" => "2456598c88d3fdd8aa000017",
+        "chat_room_id" => "33398950c711636832000059",
+        "data" => %{},
+        "importance" => "normal",
+        "is_deleted" => false,
+        "message" => "脱輪",
+        "msg_seq" => 10230,
+        "post_date" => "2015-07-22T00:57:45Z",
+        "request_id" => "d80979c1-624c-4b9e-91e3-d1a0a713c7d01437526664419",
+        "type" => "string",
+        "unread_count" => 1,
+        "user_id" => "1267712707"
+      },
+      "server_time" => "2015-07-22T00:57:45Z"
+    }
 
-#     # リクエストに対し正常レスポンスが返ることを期待するテストコードを実装してください。
-#   end
-# end
+    now = ~U[2021-06-02T03:53:30Z]
+    :meck.expect(DateTime, :utc_now, fn -> now end)
+
+    [
+      {"jamming", "異物混入"},
+      {"derailment", "脱輪"},
+      {"dead_battery", "バッテリー不足"},
+    ]
+    |> Enum.each(fn {alert_message, linkit_message} ->
+      :meck.expect(Linkit, :post_message, fn message ->
+        assert message == linkit_message
+        {201, put_in(res_body, ["chat_message", "message"], linkit_message)}
+      end)
+
+      req_body = %{"type" => alert_message}
+      res = Req.post_json(@api_path, req_body, %{})
+      assert res.status == 200
+      assert Jason.decode!(res.body) == %{"sent_at" => "2021-06-02T03:53:30Z"}
+    end)
+  end
+
+  test "alert api fails with 400" do
+    expected_body = %{"message" => "Unable to understand the request", "type" => "BadRequest"}
+
+    Enum.each(["unexpected", 0], fn message ->
+      req_body = %{"type" => message}
+      res = Req.post_json(@api_path, req_body, %{})
+      assert res.status == 400
+      assert Jason.decode!(res.body) == expected_body
+    end)
+  end
+
+  test "alert api fails with 500 due to status 404 of Linkit API" do
+    :meck.expect(Httpc, :post, 3, {:ok, %{status: 404}})
+
+    expected_body = %{"message" => "Error caused on Linkit", "type" => "LinkitError"}
+
+    req_body = %{"type" => "jamming"}
+    res = Req.post_json(@api_path, req_body, %{})
+    assert res.status == 500
+    assert Jason.decode!(res.body) == expected_body
+  end
+
+  test "alert api fails with 500 due to timeout of Linkit API" do
+    :meck.expect(Httpc, :post, 3, {:error, :timeout})
+
+    expected_body = %{"message" => "Error caused on Linkit", "type" => "LinkitError"}
+
+    req_body = %{"type" => "jamming"}
+    res = Req.post_json(@api_path, req_body, %{})
+    assert res.status == 500
+    assert Jason.decode!(res.body) == expected_body
+  end
+end
