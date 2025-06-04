e1b3248db1f2bf5141b155c43bce8b0b83f3291b
Rui.Fujii <rui.fujii@access-company.com>
2025-06-04 17:17:45 +0900

    feat: impl task 2

--- web/controller/alert.ex
+++ web/controller/alert.ex
@@ -3,11 +3,15 @@ defmodule IotIntern.Controller.Alert do
 
   alias Antikythera.Conn
   # 必要に応じて適宜エイリアスのコメントアウトを解除してください
-  # alias IotIntern.Error
+  alias IotIntern.Error
   # alias IotIntern.Linkit
 
-  def post_alert(%{request: %{body: _body}} = conn) do
-    iso_now_time = DateTime.utc_now() |> DateTime.truncate(:second) |> DateTime.to_iso8601()
-    Conn.json(conn, 200, %{sent_at: iso_now_time})
+  def post_alert(%{request: %{body: body}} = conn) do
+    if body["type"] in ["dead_battery", "derailment", "jamming"] do
+      iso_now_time = DateTime.utc_now() |> DateTime.truncate(:second) |> DateTime.to_iso8601()
+      Conn.json(conn, 200, %{sent_at: iso_now_time})
+    else
+      Conn.json(conn, 400, Error.bad_request_error())
+    end
   end
 end
